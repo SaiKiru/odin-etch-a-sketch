765b1857f5673b1fe0c9f476d95641d096671550
Anthony Aromin <menekoiskawaii@gmail.com>
2022-04-20 00:20:05 +0800

    Add painting behavior

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ function fillGrid(grid, size = 16) {
 function createSquare() {
   const square = document.createElement('div');
   square.classList.add('square');
+  square.addEventListener('mouseover', (e) => paint(e.target));
 
   return square;
 }
@@ -26,6 +27,24 @@ function createRow() {
   row.classList.add('row');
 
   return row;
+}
+
+function paint(square) {
+  if (square.classList.contains('painted-10')) {
+    square.classList.remove('painted-10');
+    square.classList.add('painted-20');
+  } else if (square.classList.contains('painted-20')) {
+    square.classList.remove('painted-20');
+    square.classList.add('painted-30');
+  } else if (square.classList.contains('painted-30')) {
+    square.classList.remove('painted-30');
+    square.classList.add('painted-40');
+  } else if (square.classList.contains('painted-40')) {
+    square.classList.remove('painted-40');
+    square.classList.add('painted-50');
+  } else {
+    square.classList.add('painted-10');
+  }
 }
 
 fillGrid(canvas, 16);
